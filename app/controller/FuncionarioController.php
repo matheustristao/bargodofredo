fbe1a30471ca1328dd3865c803238b289baf4ac7
matheustristao <matheus.tristao@gmail.com>
2014-09-03 00:13:31 -0300

    Comentários na classe FuncionarioController

--- app/controller/FuncionarioController.php
+++ app/controller/FuncionarioController.php
@@ -1,10 +1,14 @@
 <?php
 
+//Classe FuncionarioController, responsável por controlar funcões de Funcionario
 class FuncionarioController {
 
+    //Variáveis que recebem instancia das classes FuncionarioDao e Funcionario
     private static $funcionarioDao;
     private static $funcionario;
 
+    //Aqui é utilizado singleton, de forma que a classe só poderá ser instanciada uma vez
+    //Sempre que a instancia for usada, ela virá como um retorno dessa funcao
     private function instanciaFuncionario() {
 
         if (!isset(self::$funcionario)) {
@@ -21,12 +25,19 @@ class FuncionarioController {
         return self::$funcionarioDao;
     }
 
+    
+    //Essa funcao retorna uma variavel lista, que recebe o retorno da funcao listFuncionario() 
+    //da classe FuncionarioDao
     public function listarFuncionarios() {
 
         $funcionarioDao = self::instanciaFuncionarioDao();
 
         return $lista = $funcionarioDao->listFuncionario();
     }
+    
+    //Essa funcao cadastra um funcionario, setando os valores recebidos na classe Funcionario
+    // e depois chamando a funcao salvarFuncionario da classe FuncionarioDao, enviando
+    // como parametros os valores setados na classe Funcionario
 
     public function cadastrarFuncionario($nome, $cpf, $sexo, $ano_nascimento, $salario, $carteira_trabalho) {
 
